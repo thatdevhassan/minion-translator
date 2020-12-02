cd84517c8833f1a709f0e19a21d87fb98213e623
thatdevhassan <thatdevhassan@gmail.com>
2020-12-02 16:36:27 -0500

    fixing

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-var name = prompt("what is your name? ");
+// declaring variables. attaching to ids.
+var btnTranslate = document.querySelector("#trans-button");
+var intext = document.querySelector("#in-text");
+var outtext = document.querySelector("#out-text");
 
-alert("the entered name is " + name);
+
+
+// checking for click
+function clickfunc(){
+    // console.log("the button is clicked");
+    // console.log(intext.value);
+    outtext.innerText = intext.value.length;
+    }
+
+btnTranslate.addEventListener("click", clickfunc);
+
+// outtext.innerText = intext.value;
+
